7f8b94732b4a14f7ad11b09722a10564afedf373
5rtec <5rtec.main@gmail.com>
2025-08-12 10:59:18 +0500

    changed admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Material, Sellor
+from .models import MaterialCategory, Sellor
 
 # Register your models here.
-admin.site.register(Material)
+admin.site.register(MaterialCategory)
 admin.site.register(Sellor)
